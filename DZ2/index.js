8c29efbcf98c4f70e19fd4da325b3a3838d0ee85
ayurev87 <a-yurev_87@mail.ru>
2023-07-29 14:18:30 +0300

    add js

---
+++ DZ2/index.js
@@ -0,0 +1,11 @@
+let celsius = 10;
+function transformationFahrenheit (a){
+    let Fahrenheit;
+    return Fahrenheit = 9 / 5 * a + 32;
+}
+
+console.log(`${celsius} градусов Цельсия  = ${transformationFahrenheit(celsius) } градусов Фаренгейта`);
+
+const name = 'Alex';
+const Admin = name;
+console.log(Admin);
--- seminar1/script.js
+++ seminar1/script.js
@@ -1,4 +1,52 @@
 console.log("Текст из script.js");
-console.log("Текст из script.js");
-console.log("Текст из script.js");
-console.log("Текст из script.js");
+/* 
+const num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(`a = ${a}`);
+
+let b;
+ console.log (`b = ${1+2+3}`);
+
+let [aa, bb] = [1, 2];
+console.log(aa, bb); */
+
+//  1 комментарий
+ /* 2 комментарий */
+/* 
+ const c = 10;
+ const d = 2;
+
+ const add = c+d;
+ const sub = c-d;
+ const div = c/d;
+ const multi = c*d;
+ console.log(`add = ${add}
+ sub = ${sub}
+  multi = ${multi}
+ div = ${div}`);
+
+ const res = add + sub + div + multi;
+  console.log('res = ' + res); */
+
+/*   let a = 1.5;
+  let b = 0.75;
+  const res = a  +  b;
+  console.log('res = ' +res);
+  a = 20;
+  console.log( 'a = ' + a); */
+  
+  const firstName = 'Алексей';
+  const lasteName = 'Юреьв';
+  console.log(`ваш имя - ${firstName}
+Ваш фамилия - ${lasteName}`);
+
+  const first = 'java';
+  const second = 'script';
+  console.log(first+second);
+
+  const firstText = 'Hello';
+  const secondText = 'world';
+  console.log(firstText + ' ' + secondText);
