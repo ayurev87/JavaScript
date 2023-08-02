fe58d1a17b022d550fae5cb817cc1cbc23e95766
ayurev87 <a-yurev_87@mail.ru>
2023-08-02 23:09:06 +0300

    add lession7

---
+++ lession7/script.js
@@ -0,0 +1,49 @@
+// let pass;
+// let count =0;
+
+// do  {
+//     if(count>1){
+//         alert('Пароль неверный!');
+//     }
+//     pass = Number(prompt('Введите пароль'));
+//     count ++;
+    
+// } while ( pass != 234);
+
+// const arr = [];
+// /* arr = 5; // - нельзя записать число в массиве */
+// arr.push(1);
+// console.log(arr);
+// let arrNew = [];
+// arrNew = 6;
+
+/* const arr = [1, 2, 'Hello',4,7];
+console.log(arr);
+console.log(arr[2]);
+console.log(arr.length);
+console.log(arr[arr.length -1]); */
+
+/* const user = [];
+
+const userName = prompt('Ваш имя');
+const userAge = prompt('Ваш возраст');
+
+user.push(userName);
+user.push(Number(userAge));
+
+console.log(user); */
+
+
+/* const numbers = [2, 3, 19, 15];
+ 
+alert(`Запомни цифры ${numbers}`);
+
+// console.log(number[numbers.length - 1]);
+console.log(numbers.pop());
+console.log(numbers); */
+
+const word = 'example';
+
+const arrWord = word.split('');
+
+console.log(arrWord.pop);
