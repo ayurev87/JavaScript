e7216abfd3df2853272063bdccc8865a9a9245ae
ayurev87 <a-yurev_87@mail.ru>
2023-08-07 00:41:32 +0300

    new dz10 & add zad1

---
+++ DZ10/script.js
@@ -0,0 +1,15 @@
+//Задание 1
+console.log('Задание 1\n');
+
+const numbers = {
+    keyin1 : 1,
+    keyin2 : 2,
+    keyin3 : 3,
+    keyin4 : 4,
+    keyin5 : 5,
+    keyin6 : 6,
+    keuin7 : 7,
+}
+
+console.log(numbers);
+
